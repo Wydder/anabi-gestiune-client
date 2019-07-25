8486b67545f6db3c52e7d2e52831f110ece01a64
wydder <bogdan.mosica@gmail.com>
2019-07-25 19:14:54 +0300

    fake commit to change branch

--- src/app/assets/components/view-address/view-address.component.ts
+++ src/app/assets/components/view-address/view-address.component.ts
@@ -1,6 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Address } from '@app/core';
 
+export enum AddressState {
+  View = 'view',
+  Edit = 'edit',
+}
+
 @Component({
   selector: 'app-view-address',
   templateUrl: './view-address.component.html',
@@ -8,4 +13,26 @@ import { Address } from '@app/core';
 })
 export class ViewAddressComponent {
   @Input() address: Address;
+
+  private state: AddressState = AddressState.View;
+
+  isStateView(): boolean {
+    return this.state === AddressState.View;
+  }
+
+  isStateEdit(): boolean {
+    return this.state === AddressState.Edit;
+  }
+
+  editAddress() {
+    this.setStateEdit();
+  }
+
+  private setStateEdit() {
+    this.state = AddressState.Edit;
+  }
+
+  private setStateView() {
+    this.state = AddressState.View;
+  }
 }
